d1c683cbce0987937f37c9a93c4a1257cd1a016a
Estudiantes <Estudiantes@labing50404.udistrital.edu.co>
2017-10-06 11:37:48 -0500

    Creada clase Sandbox con sus respectivos metodos

--- Unir.php
+++ Unir.php
@@ -2,9 +2,5 @@
 echo $_REQUEST["novio"]." ama a ";
 echo $_REQUEST["novia"];
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
